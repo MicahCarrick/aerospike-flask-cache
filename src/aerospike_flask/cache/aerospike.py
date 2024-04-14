373941e50f75b271ea7296ab35d53229ca370886
Micah Carrick <micahcarrick@gmail.com>
2024-04-14 09:29:01 +0200

    Add tests for implemented methods

--- src/aerospike_flask/cache/aerospike.py
+++ src/aerospike_flask/cache/aerospike.py
@@ -46,18 +46,6 @@ class AerospikeCache(BaseCache):
         except ImportError as err:
             raise RuntimeError("aerospike module not found") from err
 
-        # instantiated client takes precedence over hosts array
-        if "CACHE_AEROSPIKE_CLIENT" in config:
-            kwargs["client"] = config.get("CACHE_AEROSPIKE_CLIENT", None)
-        elif "CACHE_AEROSPIKE_HOSTS" in config:
-            asconf = {
-                'hosts': config.get("CACHE_AEROSPIKE_HOSTS")
-            }
-            kwargs["client"] = aerospike.client(asconf)
-            logger.debug("Initialized client: %s", kwargs["client"])
-        else:
-            raise RuntimeError("must specify client or host")
-
         if 'CACHE_AEROSPIKE_NAMESPACE' not in config:
             msg = "CACHE_AEROSPIKE_NAMESPACE is required"
             raise RuntimeError(msg)
@@ -72,6 +60,18 @@ class AerospikeCache(BaseCache):
         if kwargs['set_name'] and len(kwargs['set_name'].encode('utf-8')) > 63:
             msg = "CACHE_AEROSPIKE_SET must be less than 64 characters"
             raise RuntimeError(msg)
+
+        # instantiated client takes precedence over hosts array
+        if "CACHE_AEROSPIKE_CLIENT" in config:
+            kwargs["client"] = config.get("CACHE_AEROSPIKE_CLIENT", None)
+        elif "CACHE_AEROSPIKE_HOSTS" in config:
+            asconf = {
+                'hosts': config.get("CACHE_AEROSPIKE_HOSTS")
+            }
+            kwargs["client"] = aerospike.client(asconf)
+            logger.debug("Initialized client: %s", kwargs["client"])
+        else:
+            raise RuntimeError("must specify client or host")
 
         return cls(*args, **kwargs)
 
@@ -116,7 +116,7 @@ class AerospikeCache(BaseCache):
                  connected.
         :rtype: boolean
         """
-        if self._client is not None and self._client.is_connected():
+        if self.is_connected():
             logger.debug("closing Aerospike client: %s", self._client)
             self._client.close()
             self._client = None
@@ -206,6 +206,18 @@ class AerospikeCache(BaseCache):
         except self._aerospike.exception.RecordNotFound:
             logger.debug("Cache miss on key: %s", key)
             return None
+
+    def is_connected(self):
+        """Check if the Aerospike client is initialized and connected to the
+        database.
+
+        :returns: ``True`` if the Aerospike client is connected
+        :rtype: boolean
+        """
+        if self._client is not None and self._client.is_connected():
+            return True
+
+        return False
 
     def get_metadata(self, key):
         """Look up key in the cache and return the Aerospike record metadata.
@@ -303,4 +315,3 @@ class AerospikeCache(BaseCache):
             if self.set(key, value, timeout):
                 set_keys.append(key)
         return set_keys
-
